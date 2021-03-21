a6df01bba0aefb9a8599a848eecc1dda8b5c870f
David Sierra <dasies11@gmail.com>
2021-03-21 10:51:19 +0100

    Create mower use case to handle unformatted string values and convert them to domain values

---
+++ src/main/kotlin/com/smart/mower/application/usecase/CreateMower.kt
@@ -0,0 +1,44 @@
+package com.smart.mower.application.usecase
+
+import com.smart.mower.application.service.InstructionValidatorService
+import com.smart.mower.domain.mower.Command
+import com.smart.mower.domain.mower.Mower
+import com.smart.mower.domain.mower.Orientation
+import com.smart.mower.domain.mower.Position
+import com.smart.mower.domain.plateau.PlateauSize
+
+private const val DELIMITER = " "
+
+class CreateMower(private val validator: InstructionValidatorService) {
+
+    private val orientations: Map<String, Orientation> =
+        mapOf("N" to Orientation.NORTH, "S" to Orientation.SOUTH, "E" to Orientation.EAST, "W" to Orientation.WEST)
+
+    private val commands: Map<Char, Command> =
+        mapOf('L' to Command.LEFT, 'R' to Command.RIGHT, 'M' to Command.FORWARD)
+
+    operator fun invoke(coordinates: String, commandValues: String, plateauSize: PlateauSize): Mower {
+        return buildMower(coordinates, commandValues, plateauSize)
+    }
+
+    private fun buildMower(coordinates: String, commandValues: String, plateauSize: PlateauSize): Mower {
+        validator.validatePosition(coordinates)
+        validator.validateCommands(commandValues)
+
+        val positions = coordinates.split(DELIMITER)
+        val position = Position(positions[0].toInt(), positions[1].toInt())
+        val orientation = orientations.getOrDefault(positions[2], Orientation.NORTH)
+        val commands = getCommands(commandValues)
+
+        return Mower(position, orientation, commands, plateauSize)
+    }
+
+    private fun getCommands(commandValues: String): MutableList<Command> {
+        val commands = mutableListOf<Command>()
+        commandValues.forEach {
+            this.commands[it]?.let(commands::add)
+        }
+        return commands
+    }
+
+}
