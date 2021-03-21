122f54a54b72e8c4316dc2c81f4cdf6e8db6b4ee
David Sierra <dasies11@gmail.com>
2021-03-21 10:15:52 +0100

    Add plateau domain with validation in constructor

--- src/main/kotlin/com/smart/mower/domain/plateau/Plateau.kt
+++ src/main/kotlin/com/smart/mower/domain/plateau/Plateau.kt
@@ -2,4 +2,9 @@ package com.smart.mower.domain.plateau
 
 class Plateau(val plateauId: PlateauId, val width: Int, val height: Int) {
 
+    init {
+        if (width < 0) throw PlateauException("Width grid size should be positive")
+        if (height < 0) throw PlateauException("Height grid size should be positive")
+    }
+
 }
