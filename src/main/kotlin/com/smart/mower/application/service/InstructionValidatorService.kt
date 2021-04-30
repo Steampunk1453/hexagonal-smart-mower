c651846018a1c1d95edc1c69a7989460f18d335e
David Sierra <dasies11@gmail.com>
2021-04-30 18:39:54 +0200

    Change format

--- src/main/kotlin/com/smart/mower/application/service/InstructionValidatorService.kt
+++ src/main/kotlin/com/smart/mower/application/service/InstructionValidatorService.kt
@@ -7,18 +7,24 @@ private const val COMMAND_FORMAT_PATTERN = "^([LRM]+)$"
 class InstructionValidatorService {
 
     fun validateGrid(grid: String) {
-        check(grid.matches(GRID_FORMAT_PATTERN.toRegex())) { "Invalid format for these grid dimensions: $grid." +
-                " Format should have correct width and height" }
+        check(grid.matches(GRID_FORMAT_PATTERN.toRegex())) {
+            "Invalid format for these grid dimensions: $grid." +
+                    " Format should have correct width and height"
+        }
     }
 
     fun validatePosition(position: String) {
-        check(position.matches(POSITION_FORMAT_PATTERN.toRegex())) { "Invalid format for these positions: $position." +
-                " Format should have X Y and compass point (N|E|S|W)" }
+        check(position.matches(POSITION_FORMAT_PATTERN.toRegex())) {
+            "Invalid format for these positions: $position." +
+                    " Format should have X Y and compass point (N|E|S|W)"
+        }
     }
 
     fun validateCommands(commands: String) {
-        check(commands.matches(COMMAND_FORMAT_PATTERN.toRegex())) { "Invalid format for these commands: $commands." +
-                " Format should have any of these letters: L|R|M" }
+        check(commands.matches(COMMAND_FORMAT_PATTERN.toRegex())) {
+            "Invalid format for these commands: $commands." +
+                    " Format should have any of these letters: L|R|M"
+        }
     }
 
 }
